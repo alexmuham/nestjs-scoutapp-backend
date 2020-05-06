504db56b07f5181f9ef2dbc818aa504a7f389ef1
danil_klushnik <danilklushnilworking@gmail.com>
2020-05-06 17:58:33 +0300

    add functionality

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native-community', //TODO fixed
+    'airbnb-typescript', //TODO fixed
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/named': 'off',
+    'import/order': 'off',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'no-shadow': 'off',
+    'default-case': 'off',
+    'consistent-return': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-useless-return': 'off',
+    'no-await-in-loop': 'off',
+    'import/no-cycle': 'error',
+    'no-console': 'error',
+    'react-native/no-inline-styles': 'error'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        helpers: true,
+        regenerator: false,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        root: ['./'],
+        alias: {},
+      },
+    ],
+  ],
 };
