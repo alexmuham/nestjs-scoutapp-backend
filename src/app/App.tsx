64f9368f84f9e384ed50e64bf5ee4c17afecc436
danil_klushnik <danilklushnilworking@gmail.com>
2020-05-09 12:16:15 +0300

    add registration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,46 @@
 import React from 'react';
-import {Store} from 'redux';
 import {Provider} from 'react-redux';
 import {getStore} from 'state';
 import {NativeRouter, Route, Link} from 'react-router-native';
 import {Text, View} from 'react-native';
 import {Registration} from '../screens/auth';
+import LogIn from '../screens/auth/login/LogIn';
+import ForgotPassword from '../screens/auth/forgotPassword/ForgotPassword';
+import {I18nextProvider} from 'react-i18next';
+import i18n from 'i18next';
+import {getBuildType} from 'services/config/ConfigUtils';
+import BuildType from 'entities/BuildType';
+import AppDebugHeader from 'app/debug/AppDebugHeader';
 
 const App: React.FC = () => {
+  const buildType = getBuildType();
+  const isDebug = buildType !== BuildType.Production;
+
   return (
-    <Registration />
-    // <Text>TEXT</Text>
-    // <Provider store={getStore()} />
-    // <NativeRouter>
-    //   <View>
-    //     <View>
-    //       <Link to="/" underlayColor="#f0f4f7">
-    //         <Text>Home</Text>
-    //       </Link>
-    //       <Link to="/about" underlayColor="#f0f4f7">
-    //         <Text>About</Text>
-    //       </Link>
-    //       <Link to="/topics" underlayColor="#f0f4f7">
-    //         <Text>Topics</Text>
-    //       </Link>
-    //     </View>
-    //
-    //     <Route exact path="/" component={registration} />
-    //   </View>
-    // </NativeRouter>
-    // </Provider>
+    <Provider store={getStore()}>
+      <I18nextProvider i18n={i18n}>
+        <NativeRouter>
+          <View>
+            <View>
+              <Link to="/" underlayColor="#f0f4f7">
+                <Text>LogIn</Text>
+              </Link>
+              <Link to="/registration" underlayColor="#f0f4f7">
+                <Text>Registration</Text>
+              </Link>
+              <Link to="/forgotPassword" underlayColor="#f0f4f7">
+                <Text>ForgotPassword</Text>
+              </Link>
+            </View>
+
+            <Route exact path="/" component={LogIn} />
+            <Route exact path="/registration" component={Registration} />
+            <Route exact path="/forgotPassword" component={ForgotPassword} />
+          </View>
+        </NativeRouter>
+      </I18nextProvider>
+      {isDebug && <AppDebugHeader buildType={buildType} />}
+    </Provider>
   );
 };
 
