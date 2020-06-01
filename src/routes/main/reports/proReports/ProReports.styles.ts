18036a20e30ac585c302f7a5366402938702e4f7
danil_klushnik <danilklushnilworking@gmail.com>
2020-06-01 13:08:43 +0300

    add parameter to pro reports

--- src/routes/main/reports/proReports/ProReports.styles.ts
+++ src/routes/main/reports/proReports/ProReports.styles.ts
@@ -7,6 +7,98 @@ const styles = StyleSheet.create({
   reportHeader: {
     paddingHorizontal: 20,
   },
+  textInput: {
+    paddingLeft: 9,
+    textAlign: 'center',
+    backgroundColor: '#FFF',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    borderRadius: 5,
+    paddingRight: 10,
+    width: 50,
+    height: 40,
+  },
+  parameterItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  parameterItemScrollView: {
+    paddingBottom: 18,
+    paddingTop: 2,
+  },
+  parameterTextInputContainer: {
+    paddingRight: 20,
+  },
+  parameterItemTitle: {
+    width: 135,
+    fontSize: 18,
+  },
+  parameterItemsTitle: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+  },
+  parameterItemsContainer: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  textParameter: {
+    width: 110,
+  },
+  textP: {
+    width: 95,
+    textAlign: 'center',
+  },
+  textF: {
+    width: 50,
+    textAlign: 'center',
+  },
+  textAdj: {
+    width: 90,
+    textAlign: 'center',
+  },
+  parameterItemContainer: {
+    paddingTop: 20,
+  },
+  textInputColorTwo: {
+    color: '#000000',
+    borderColor: '#000000',
+    borderWidth: 1,
+  },
+  textInputColorTree: {
+    color: '#A61911',
+    borderColor: '#A61911',
+    borderWidth: 1,
+  },
+  textInputColorFore: {
+    color: '#FEB72D',
+    borderColor: '#FEB72D',
+    borderWidth: 1,
+  },
+  textInputColorFife: {
+    color: '#00B13C',
+    borderColor: '#00B13C',
+    borderWidth: 1,
+  },
+  textInputColorSix: {
+    color: '#2DB3FE',
+    borderColor: '#2DB3FE',
+    borderWidth: 1,
+  },
+  textInputColorSeven: {
+    color: '#FEE92D',
+    borderColor: '#FEE92D',
+    borderWidth: 1,
+  },
+  textInputColorAit: {
+    borderColor: '#FFFFFF',
+    borderWidth: 1,
+  },
 });
 
 export default styles;
--- src/routes/main/reports/proReports/ProReports.tsx
+++ src/routes/main/reports/proReports/ProReports.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {ScrollView, View} from 'react-native';
+import {FlatList, ScrollView, Text, TextInput, View} from 'react-native';
 import styles from './ProReports.styles';
 import {ReportsHeader, ReportsInput, RoleFuturePositionRound} from 'components';
 import {useRouterActions} from 'state/hooks/UseActions';
@@ -22,6 +22,209 @@ const ProReports: React.FC = () => {
 
   const [physicalDest, setPhysicalDest] = useState<string>('');
   const [playerComp, setPlayerComp] = useState<string>('');
+
+  const [hittingAbilityP, setHittingAbilityP] = useState<string>('');
+  const [hittingAbilityF, setHittingAbilityF] = useState<string>('');
+  const [hittingAbilityAdj, setHittingAbilityAdj] = useState<string>('');
+
+  const [hitAppTypeP, setHitAppTypeP] = useState<string>('');
+  const [hitAppTypeF, setHitAppTypeF] = useState<string>('');
+  const [hitAppTypeAbj, setHitAppTypeAbj] = useState<string>('');
+
+  const [powerFreqP, setPowerFreqP] = useState<string>('');
+  const [powerFreqF, setPowerFreqF] = useState<string>('');
+  const [powerFreqAbj, setPowerFreqAbj] = useState<string>('');
+
+  const [rawPwrP, setRawPwrP] = useState<string>('');
+  const [rawPwrF, setRawPwrF] = useState<string>('');
+  const [rawPwrAbj, setRawPwrAbj] = useState<string>('');
+
+  const [runningAbilityP, setRunningAbilityP] = useState<string>('');
+  const [runningAbilityF, setRunningAbilityF] = useState<string>('');
+  const [runningAbilityAbj, setRunningAbilityAbj] = useState<string>('');
+
+  const [baseStealerP, setBaseStealerP] = useState<string>('');
+  const [baseStealerF, setBaseStealerF] = useState<string>('');
+  const [baseStealerAbj, setBaseStealerAbj] = useState<string>('');
+
+  const [armStrengthP, setArmStrengthP] = useState<string>('');
+  const [armStrengthF, setArmStrengthF] = useState<string>('');
+  const [armStrengthAbj, setArmStrengthAbj] = useState<string>('');
+
+  const [fieldingAbilityP, setFieldingAbilityP] = useState<string>('');
+  const [fieldingAbilityF, setFieldingAbilityF] = useState<string>('');
+  const [fieldingAbilityAbj, setFieldingAbilityAbj] = useState<string>('');
+
+  const [handsP, setHandsP] = useState<string>('');
+  const [handsF, setHandsF] = useState<string>('');
+  const [handsAbj, setHandsAbj] = useState<string>('');
+
+  const [rangeP, setRangeP] = useState<string>('');
+  const [rangeF, setRangeF] = useState<string>('');
+  const [rangeAbj, setRangeAbj] = useState<string>('');
+
+  const [feetP, setFeetP] = useState<string>('');
+  const [feetF, setFeetF] = useState<string>('');
+  const [feetAbj, setFeetAbj] = useState<string>('');
+
+  const [canHePlayP, setCanHePlayP] = useState<string>('');
+  const [canHePlayF, setCanHePlayF] = useState<string>('');
+  const [canHePlayAbj, setCanHePlayAbj] = useState<string>('');
+
+  const parameterByPlayer = [
+    {
+      title: 'hittingAbility',
+      p: hittingAbilityP,
+      setP: (value: string) => setHittingAbilityP(value),
+      f: hittingAbilityF,
+      setF: (value: string) => setHittingAbilityF(value),
+      adj: hittingAbilityAdj,
+      setAdj: (value: string) => setHittingAbilityAdj(value),
+    },
+    {
+      title: 'hitAppType',
+      p: hitAppTypeP,
+      setP: (value: string) => setHitAppTypeP(value),
+      f: hitAppTypeF,
+      setF: (value: string) => setHitAppTypeF(value),
+      adj: hitAppTypeAbj,
+      setAdj: (value: string) => setHitAppTypeAbj(value),
+    },
+    {
+      title: 'powerFreq',
+      p: powerFreqP,
+      setP: (value: string) => setPowerFreqP(value),
+      f: powerFreqF,
+      setF: (value: string) => setPowerFreqF(value),
+      adj: powerFreqAbj,
+      setAdj: (value: string) => setPowerFreqAbj(value),
+    },
+    {
+      title: 'rawPwr',
+      p: rawPwrP,
+      setP: (value: string) => setRawPwrP(value),
+      f: rawPwrF,
+      setF: (value: string) => setRawPwrF(value),
+      adj: rawPwrAbj,
+      setAdj: (value: string) => setRawPwrAbj(value),
+    },
+    {
+      title: 'runningAbility',
+      p: runningAbilityP,
+      setP: (value: string) => setRunningAbilityP(value),
+      f: runningAbilityF,
+      setF: (value: string) => setRunningAbilityF(value),
+      adj: runningAbilityAbj,
+      setAdj: (value: string) => setRunningAbilityAbj(value),
+    },
+    {
+      title: 'baseStealer',
+      p: baseStealerP,
+      setP: (value: string) => setBaseStealerP(value),
+      f: baseStealerF,
+      setF: (value: string) => setBaseStealerF(value),
+      adj: baseStealerAbj,
+      setAdj: (value: string) => setBaseStealerAbj(value),
+    },
+    {
+      title: 'armStrength',
+      p: armStrengthP,
+      setP: (value: string) => setArmStrengthP(value),
+      f: armStrengthF,
+      setF: (value: string) => setArmStrengthF(value),
+      adj: armStrengthAbj,
+      setAdj: (value: string) => setArmStrengthAbj(value),
+    },
+    {
+      title: 'fieldingAbility',
+      p: fieldingAbilityP,
+      setP: (value: string) => setFieldingAbilityP(value),
+      f: fieldingAbilityF,
+      setF: (value: string) => setFieldingAbilityF(value),
+      adj: fieldingAbilityAbj,
+      setAdj: (value: string) => setFieldingAbilityAbj(value),
+    },
+    {
+      title: 'hands',
+      p: handsP,
+      setP: (value: string) => setHandsP(value),
+      f: handsF,
+      setF: (value: string) => setHandsF(value),
+      adj: handsAbj,
+      setAdj: (value: string) => setHandsAbj(value),
+    },
+    {
+      title: 'range',
+      p: rangeP,
+      setP: (value: string) => setRangeP(value),
+      f: rangeF,
+      setF: (value: string) => setRangeF(value),
+      adj: rangeAbj,
+      setAdj: (value: string) => setRangeAbj(value),
+    },
+    {
+      title: 'feet',
+      p: feetP,
+      setP: (value: string) => setFeetP(value),
+      f: feetF,
+      setF: (value: string) => setFeetF(value),
+      adj: feetAbj,
+      setAdj: (value: string) => setFeetAbj(value),
+    },
+    {
+      title: 'canHePlay',
+      p: canHePlayP,
+      setP: (value: string) => setCanHePlayP(value),
+      f: canHePlayF,
+      setF: (value: string) => setCanHePlayF(value),
+      adj: canHePlayAbj,
+      setAdj: (value: string) => setCanHePlayAbj(value),
+    },
+  ];
+
+  const itemColor = (item: string) => {
+    switch (item) {
+      case '2': {
+        return {...styles.textInput, ...styles.textInputColorTwo};
+      }
+      case '3': {
+        return {...styles.textInput, ...styles.textInputColorTree};
+      }
+      case '4': {
+        return {...styles.textInput, ...styles.textInputColorFore};
+      }
+      case '5': {
+        return {...styles.textInput, ...styles.textInputColorFife};
+      }
+      case '6': {
+        return {...styles.textInput, ...styles.textInputColorSix};
+      }
+      case '7': {
+        return {...styles.textInput, ...styles.textInputColorSeven};
+      }
+      case '8': {
+        return {...styles.textInput, ...styles.textInputColorAit};
+      }
+      default: {
+        return {...styles.textInput};
+      }
+    }
+  };
+
+  const renderParameterItem = (value: string, setValue: (value: string) => void) => {
+    return (
+      <View style={styles.parameterTextInputContainer}>
+        <TextInput
+          underlineColorAndroid="transparent"
+          placeholderTextColor="#929292"
+          style={itemColor(value)}
+          onChangeText={setValue}
+          value={value}
+          maxLength={1}
+        />
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -72,6 +275,34 @@ const ProReports: React.FC = () => {
           value={playerComp}
           setValue={setPlayerComp}
         />
+        <View style={styles.parameterItemsContainer}>
+          <View style={styles.parameterItemsTitle}>
+            <Text style={styles.textParameter}>{t('parameter')}</Text>
+            <Text style={styles.textP}>{t('p')}</Text>
+            <Text style={styles.textF}>{t('f')}</Text>
+            <Text style={styles.textAdj}>{t('adj')}</Text>
+          </View>
+          <View style={styles.parameterItemContainer}>
+            <FlatList
+              data={parameterByPlayer}
+              renderItem={({item}) => {
+                const {adj, f, p, title, setAdj, setF, setP} = item;
+                return (
+                  <View style={styles.parameterItem}>
+                    <ScrollView style={styles.parameterItemScrollView} horizontal>
+                      <View style={styles.parameterItem}>
+                        <Text style={styles.parameterItemTitle}>{title}</Text>
+                        {renderParameterItem(p, setP)}
+                        {renderParameterItem(f, setF)}
+                        {renderParameterItem(adj, setAdj)}
+                      </View>
+                    </ScrollView>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        </View>
       </ScrollView>
     </View>
   );
